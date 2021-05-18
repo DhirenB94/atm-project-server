7ef38d2461412f971cf6fa9e0d4ec7aa7db6773e
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-05-18 17:50:46 +0100

    pins route modified to get username from backend

--- routes/pins.js
+++ routes/pins.js
@@ -4,9 +4,15 @@ const Pin = require("../models/Pin");
 
 //create a pin
 router.post("/", async (req, res) => {
-  const newPin = new Pin(req.body);
+  const {name, comment, lat, long} = req.body;
   try {
-    const savedPin = await newPin.save();
+    const savedPin = await Pin.create({
+      name,
+      comment,
+      lat,
+      long,
+      username: req.user.username
+    });
     res.status(200).json(savedPin);
   } catch (err) {
     res.status(500).json(err);
